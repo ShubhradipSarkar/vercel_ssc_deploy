f6e981bff5be50f6dc35ad7c10d3c4bd038ac292
shubhradip <shubhradipsarkar@gmail.com>
2023-03-22 19:03:28 +0530

    slides resized

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -10,6 +10,7 @@ import skybg from '../../src/images/pgbg.jpg';
 function UncontrolledExample() {
   return (
     <Carousel>
+      
       <Carousel.Item>
         <img
           className="d-block w-100"
